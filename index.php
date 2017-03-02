3c02332972b4e10e3bea682d9a3da1aa72b8238c
gaurangsnh <gaurang.snh@outlook.com>
2017-03-03 00:38:41 +0530

    v1.0.0

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sporcon.com</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/index.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#page-top">Sporcon</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">Login</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">Write an article</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#">Search</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Home</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Home</div>
+					    </div>
+					  </div>
+					 </a>
+				</div>
+				<div class="col-lg-2">				
+					 <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Cricket</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Cricket</div>
+					    </div>
+					  </div>
+					 </a>
+				</div>
+				<div class="col-lg-2">
+					 <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Football</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Football</div>
+					    </div>
+					  </div>
+					 </a>
+				</div>
+				<div class="col-lg-2">
+					 <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Hockey</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Hockey</div>
+					    </div>
+					  </div>
+					 </a>
+				</div>
+				<div class="col-lg-2">
+					 <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Tennis</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Tennis</div>
+					    </div>
+					  </div>
+					 </a>
+                </div>
+                <div class="col-lg-2">
+					 <a href="#">
+					  <div class="button-fill grey">
+					    <div class="button-text">Badminton</div>
+					    <div class="button-inside">
+					      <div class="inside-text">Badminton</div>
+					    </div>
+					  </div>
+					 </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+ 
+     <!-- Content Section -->
+    <section class="success" id="about">
+        <div class="container">
+            
+        </div>
+    </section>
+ 
+ 
+ 
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Location</h3>
+                        <p>AB Homes, Sai Baba Temple Road,
+                            <br>Kundalahalli Gate, Bangalore - 560037</p>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <ul class="list-inline">
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>About Sporcon</h3>
+                        <p>Sporcon is an all sports website, wherein users can read, write, share and post comments.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; Sporcon 2017
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/index.js"></script>
+
+</body>
+
+</html>
